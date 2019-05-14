ce1806a4592d73426817a30f6588a6e2bc1d6858
DESKTOP-F3DRVR1\Jinoo <kjw2122@naver.com>
2019-05-14 19:24:16 +0900

    서버에서 클라이언트 일련번호 처리

--- middleServer/Client.java
+++ middleServer/Client.java
@@ -3,54 +3,49 @@ package middleServer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.StringTokenizer;
 
 public class Client {
 
 	public static void main(String[] args) {
-		
 		Socket sck = null;
 		BufferedReader br = null;
 		PrintWriter pw = null;
 		boolean endFlag = false;
-		// 클라이언트의 입력값을 받을 변수 input
-		String input = null;
-		// 사용자의 아이디  
 		String id = null;
-		// 사용자의 일련번호  
-		String number = null;
+		String code = null;
 		try {
-			sck = new Socket("localhost", 0525);
+			//10.10.101.153
+			sck = new Socket("192.168.0.7", 1525);
 			pw = new PrintWriter(new OutputStreamWriter(sck.getOutputStream()));
 			br = new BufferedReader(new InputStreamReader(sck.getInputStream()));
-			
 			BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println("사용할 id를 입력하시오:(아이디숫자4자리/일련번호)");
-			input = keyboard.readLine();
-			
-			// '/'토큰을 이용해 입력 값 나누기
-			StringTokenizer tokens = new StringTokenizer(input);
-			id = tokens.nextToken("/");
-			number = tokens.nextToken("/");
-
-			
-			System.out.println("===========(일련번호: "+number+")"+id+"님의 대화창=========");
-			
-			// 서버에 id보내기
-			pw.println(input);
+			//code목록 읽어오기
+			System.out.println("코드읽어오기");
+			String str = br.readLine();
+			System.out.println(str);
+			//방번호 입력받기
+			code = keyboard.readLine();
+			System.out.println("id를 입력하시오:");
+			//id 입력받기
+			id = keyboard.readLine();
+			pw.println(code+"/"+id);//일련번호랑 id 한 스트링에 담아보내기
 			pw.flush();
-			
+			System.out.println("==========="+id+"님의 대화창=========");
+			//서버에 id보내기
+			pw.println(id);
+			pw.flush();
 			//서버로 부터 계속 읽어오는 스레드 실행
 			InputThread it = new InputThread(sck,br);
 			it.start();
 			String line = null;
 			while((line = keyboard.readLine())!=null)
 			{
-				pw.println(line);
+				pw.println(code.split("/")[0]+"/"+line);
 				pw.flush();
 				if(line.equals("quit"))
 				{
@@ -60,10 +55,8 @@ public class Client {
 				}
 			}
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally {
 			try {
@@ -99,7 +92,7 @@ class InputThread extends Thread
 				System.out.println(line);
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("시스템을 종료합니다.");
 		}finally {
 			try {
 				if(sck != null)
--- middleServer/Server.java
+++ middleServer/Server.java
@@ -8,104 +8,178 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.util.Set;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
+import java.util.Iterator.*;
 public class Server {
-	// 서버 넘버
-	static int server_number = 1;
-
+	static int room1 = 0;
+	static int room2 = 0;
+	static int room3 = 0;
+	static HashMap<String, Object> hash;
+	//static PrintWriter pwr;
 	public static void main(String[] args) {
+		//제어서버에 보낼 때 사용할 socket,pw
+		Socket controlSck = null;
+		PrintWriter pwr = null;
+		
 		try {
-			ServerSocket server = new ServerSocket(0525);
-			HashMap<String, Object> hash = new HashMap<String, Object>();
-			while (true) {
-				// 중계서버에게 서버 번호 알리기
-				System.out.println(server_number + "번 서버입니다.");
-
+			//제어서버에 socket연결
+			controlSck = new Socket("192.168.0.18",10001);
+			pwr = new PrintWriter(new OutputStreamWriter(controlSck.getOutputStream()));
+			ControlThread control = new ControlThread(controlSck, pwr);
+			control.start();
+			
+			
+			
+			ServerSocket server = new ServerSocket(1525);
+			hash = new HashMap<String, Object>();
+			//hash맵 키값불러와서 각 방마다 몇명인지 구하기
+			
+			
+			
+			while(true)
+			{
+				System.out.println("=======================");
+				System.out.println("현재 서버에   "+hash.size()+"명...");
+				//hash맵 개수 구하기
+				
+				
+				
+				/*
+				 * System.out.println("100번방:  "+room1+"명...");
+				 * System.out.println("101번방:  "+room2+"명...");
+				 * System.out.println("102번방:  "+room3+"명...");
+				 */
 				System.out.println("접속을 기다리는중...");
 				Socket sck = server.accept();
 				ChatThread chatThr = new ChatThread(sck, hash);
 				chatThr.start();
+				
+				
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
 	}
 }
-
-class ChatThread extends Thread {
-	// 일련번호를 담을 HashSet(중복 저장x)
-	HashSet<String> numberList = new HashSet<String>();
+//제어서버와 연결되는 스레드
+class ControlThread extends Thread
+{
 	Socket sck;
-	String id;
-	String number;
+	BufferedReader br;
+	PrintWriter pwr;
+	
+
+	public ControlThread(Socket sck, PrintWriter pwr) 
+	{
+		super();
+		this.sck = sck;
+		this.pwr = pwr;
+		try {
+			br = new BufferedReader(new InputStreamReader(sck.getInputStream()));
+			System.out.println(sck);
+			pwr.println("relay server/1525");
+			System.out.println("println() 실행");
+			pwr.flush();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public void run() 
+	{
+		String line = null;
+		try {
+			while((line = br.readLine()) != null)
+			{
+				System.out.println("--------제어서버: "+line);
+			
+				
+			//클라이언트로부터 quit을 받으면 종료
+				if(line.equals("quit"))
+					sck.close();
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("제어서버와 연결 끊김...");
+		}
+	}
+}
+class ChatThread extends Thread{
+	Socket sck;
+	String [] split;
+	String code;
 	BufferedReader br;
 	HashMap<String, Object> hash;
 	boolean initFlag = false;
-
-	// 서버 넘버
-	static int server_number = 1;
-
-	public ChatThread(Socket sck, HashMap<String, Object> hash) {
+	public ChatThread(Socket sck,HashMap<String, Object> hash) {
+		
 		this.sck = sck;
 		this.hash = hash;
-
 		try {
-			// id 받아오기
 			PrintWriter pw = new PrintWriter(new OutputStreamWriter(sck.getOutputStream()));
 			br = new BufferedReader(new InputStreamReader(sck.getInputStream()));
-			id = br.readLine();
-
-			// '/'토큰을 이용해 입력 값 나누기
-			StringTokenizer tokens = new StringTokenizer(id);
-			id = tokens.nextToken("/");
-			number = tokens.nextToken("/");
-
-			// nuberList에 일련번호 저장
-			numberList.add(number);
-
-			System.out.println("====(일련번호: " + number + ")" + id + "님과 성공적으로 연결");
-			broadcast(id + "님이 접속하셨습니다.");
-			synchronized (hash) {// 직렬화 후 해쉬맵에 저장
-				hash.put(this.id, pw);
-
+			pw.println("Connection is success");
+			pw.flush();
+			pw.println("Please press your code number(100, 101, 102)");
+			pw.flush();
+			System.out.println("코드보내기");
+			code = br.readLine();
+			split = code.split("/");
+			System.out.println("===="+split[1]+"님과 성공적으로 연결("+split[0]+"번방)");
+			
+				
+			broadcast(split[1]+"is connected.");
+			synchronized (hash) {//직렬화 후 해쉬맵에 저장
+				hash.put(code, pw);
 			}
 			initFlag = true;
-
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+			
 	}
-
 	public void run() {
 		String line = null;
 		try {
-			while ((line = br.readLine()) != null) {// 클라이언트로부터 quit을 받으면 종료
-				if (line.equals("quit")) {
-					System.out.println(id + "님이 시스탬을 종료합니다...");
-					// 일련번호 삭제
-					numberList.remove(number);
-
-					// 해쉬맵 하나가 사라질 경우 제어서버에게 알려줌
-					System.out.println(server_number + "번 서버 현재 접속자: " + (hash.size() - 1));
+			while((line = br.readLine()) != null)
+			{//클라이언트로부터 quit을 받으면 종료
+				if(line.split("/")[0].equals("quit"))
+				{
+					System.out.println(split[1]+"님이 시스탬을 종료합니다...");
 					break;
-
-				} else {// 아닐 경우 계속 읽어온 데이터를 클라이언트들에게 전송
-					broadcast(id + " : " + line);
+					
+				}else
+				{//아닐 경우 계속 읽어온 데이터를 클라이언트들에게 전송
+					broadcast(line);
 				}
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
+			System.out.println(split[1]+"님이 시스탬을 강제적으로 종료합니다...");
+		}finally
+		{
+			//제어서버에 "relay server/decrease" 보내기
+			
+			
+			broadcast(split[1]+"is disconnected...");
+			
 			synchronized (hash) {
-				hash.remove(id);
+				hash.remove(code);
 			}
-			broadcast(id + "님이 접속을 종료했습니다…");
+			System.out.println("=======================");
+			System.out.println("현재 서버에   "+hash.size()+"명...");
+			System.out.println("=======================");
+			//hash맵 개수 구하기
+			
+			
 			try {
 				sck.close();
 			} catch (IOException e) {
@@ -113,18 +187,25 @@ class ChatThread extends Thread {
 			}
 		}
 	}
-
-	// 메시지 전송
-	public void broadcast(String msg) {
-		synchronized (hash) {
-			Collection<Object> collec = hash.values();
-
-			java.util.Iterator<?> iter = collec.iterator();
-			while (iter.hasNext()) {
-				PrintWriter pw = (PrintWriter) iter.next();
-				pw.println(msg);
-				pw.flush();
-
+	//메시지 전송
+	public void broadcast(String msg)
+	{
+		synchronized (hash) 
+		{
+			Set<?> set = hash.keySet();
+			
+			java.util.Iterator<?> iterator = set.iterator();
+			PrintWriter pw = null;
+			while(iterator.hasNext())
+			{
+				//같은 고유번호 끼리만 메세지 주고받기
+				  String key = (String)iterator.next();
+				  if(key.split("/")[0].equals(msg.split("/")[0]))
+				  {
+					  pw = (PrintWriter)hash.get(key);
+					  pw.println(split[1]+":  "+msg.split("/")[1]);
+					  pw.flush();
+				  }
 			}
 		}
 	}
